adedf11cb5dccde9ecf4720f009b4e1079180715
Federico Savio <fsavio.3f@gmail.com>
2024-07-24 14:15:39 -0300

    a

--- R/normalizar_calles.R
+++ R/normalizar_calles.R
@@ -50,7 +50,7 @@ tokens_similitud <- function(nombre_org, nombres_normalizados) {
 
 normalizar_calles <- function(df, nombre_calles) {
   df <- df |>
-    rowwise()|>
+    dplyr::rowwise()|>
     dplyr::mutate(nombre_normalizado = tokens_similitud(!!sym(nombre_calles), diccionario_calles$nombre_simp))
   return(df)
 }  
