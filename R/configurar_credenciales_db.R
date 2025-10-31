cdff8b7884ff2b8d933e86e44a924fe03633b9c0
Federico Savio <fsavio.3f@gmail.com>
2025-10-31 09:59:33 -0300

    Update configurar_credenciales_db.R

--- R/configurar_credenciales_db.R
+++ R/configurar_credenciales_db.R
@@ -15,16 +15,7 @@ configurar_credenciales_db <- function(nombre_servicio) {
   
   message(paste("--- Configurando el servicio:", nombre_servicio, "---"))
   
-  # --- 1. Guardar el Servidor/IP ---
-  message("\n1. Guardando el SERVIDOR (IP/Hostname)...")
-  message("Por favor, introduce el valor para 'db_server' (ej. 192.168.11.32)")
-  keyring::key_set(
-    service = nombre_servicio,
-    username = "db_server" # Etiqueta para el servidor
-  )
-  message("✅ Servidor guardado.")
-  
-  # --- 2. Guardar el Usuario de la BD ---
+  # --- 1 Guardar el Usuario de la BD ---
   message("\n2. Guardando el USUARIO de la BD...")
   message("Por favor, introduce el valor para 'db_user' (ej. UsuarioVistaSSO)")
   keyring::key_set(
@@ -33,7 +24,7 @@ configurar_credenciales_db <- function(nombre_servicio) {
   )
   message("✅ Usuario guardado.")
   
-  # --- 3. Guardar la Contraseña ---
+  # --- 2. Guardar la Contraseña ---
   message("\n3. Guardando la CONTRASEÑA de la BD...")
   message("Por favor, introduce el valor para 'db_pwd'")
   keyring::key_set(
