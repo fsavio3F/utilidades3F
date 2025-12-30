f5ae45f2837bb941e2aad04836739ba75eea291b
Federico Savio <fsavio.3f@gmail.com>
2025-12-30 13:13:02 +0000

    Update actualizar_utilidades3F.R

--- R/actualizar_utilidades3F.R
+++ R/actualizar_utilidades3F.R
@@ -6,9 +6,6 @@
 #' @param ... Argumentos adicionales que se pasan a `devtools::install_github()`.
 #'
 #' @return Mensaje de instalación o error.
-#' @examples
-#' actualizar_utilidades3F()  # Instala desde rama "estable"
-#' actualizar_utilidades3F(ref = "otra-rama")  # Instala desde una rama alternativa
 #' @export
 actualizar_utilidades3F <- function(ref = "estable", ...) {
   if (!requireNamespace("devtools", quietly = TRUE)) {
